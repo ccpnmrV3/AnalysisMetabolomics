cb434db87d68b85f273f351122bf5834ba5c097e
TJ Ragan <tjr22@leicester.ac.uk>
2016-07-14 08:01:19 +0000

    Add a temporary convenience function ccpn.framework.Framework._getApplication that returns the application instance.
    git-svn-id: https://svn.code.sf.net/p/ccpn/code/trunk@9648 8c04fef6-8de3-4ed0-b809-c157a10b9597

--- __main__.py
+++ __main__.py
@@ -1,17 +1,18 @@
 __author__ = 'TJ'
 
 from ccpn.core.lib.Version import applicationVersion
-from ccpn.framework.Framework import defineProgramArguments
+from ccpn.framework import Framework
 from ccpn.AnalysisMetabolomics.AnalysisMetabolomics import Metabolomics as Application
 
 
 if __name__ == '__main__':
 
   # argument parser
-  parser = defineProgramArguments()
+  parser = Framework.defineProgramArguments()
 
   # add any additional commandline argument here
   commandLineArguments = parser.parse_args()
 
   application = Application('AnalysisMetabolomics', '3.0.a1', commandLineArguments)
+  Framework._getApplication = lambda: application
   application.start()
