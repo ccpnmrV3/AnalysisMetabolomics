f9392a8e84285ecd30daefa09c79d9864472acea
TJ Ragan <tjr22@le.ac.uk>
2017-03-09 14:17:09 +0000

    Use git commit hashes as revision(s) in mainWindow title.

--- __main__.py
+++ __main__.py
@@ -2,8 +2,11 @@ __author__ = 'TJ!'
 
 from ccpn.framework import Framework
 from ccpn.AnalysisMetabolomics.AnalysisMetabolomics import Metabolomics as Application
+from ccpn.framework.Version import applicationVersion
 
 if __name__ == '__main__':
+  from ccpn.util.GitTools import getAllRepositoriesGitCommit
+  applicationVersion = 'development: {AnalysisMetabolomics:.8s}'.format(**getAllRepositoriesGitCommit())
 
   # argument parser
   parser = Framework.defineProgramArguments()
@@ -11,6 +14,6 @@ if __name__ == '__main__':
   # add any additional commandline argument here
   commandLineArguments = parser.parse_args()
 
-  application = Application('AnalysisMetabolomics', 'ALPHA-1', commandLineArguments)
+  application = Application('AnalysisMetabolomics', applicationVersion, commandLineArguments)
   Framework._getApplication = lambda: application
   application.start()
