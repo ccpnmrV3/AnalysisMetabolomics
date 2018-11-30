bcaa2aeb289cc70412b969fbe99615a009374d2a
Ed Brooksbank <ejb66@le.ac.uk>
2018-11-30 18:47:46 +0000

    changes to sideBar updates to contextManagers minor change to overlay box for modules move of Undo.py to ccpn.core.lib rebuild of api rebuild of api documentation

--- lib/BMRBquery.py
+++ lib/BMRBquery.py
@@ -1,5 +1,31 @@
-
 """
+Module Documentation here
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2017"
+__credits__ = ("Wayne Boucher, Ed Brooksbank, Rasmus H Fogh, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license",
+               "or ccpnmodel.ccpncore.memops.Credits.CcpnLicense for licence text")
+__reference__ = ("For publications, please use reference from http://www.ccpn.ac.uk/v3-software/downloads/license",
+                 "or ccpnmodel.ccpncore.memops.Credits.CcpNmrReference")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: Luca Mureddu $"
+__dateModified__ = "$dateModified: 2017-07-07 16:32:55 +0100 (Fri, July 07, 2017) $"
+__version__ = "$Revision: 3.0.b3 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Luca Mureddu $"
+__date__ = "$Date: 2017-03-23 16:50:22 +0000 (Thu, March 23, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
+tempQueryCode = """
 Below the script from BMRB website.
 Copied for understanding how it worked only.
 
@@ -54,7 +80,7 @@ Copied for understanding how it worked only.
 
 """
 
-import pynmrstar
+# import pynmrstar
 import pandas as pd
 import urllib.request, json
 
