65267071c5d0c7c2268e2ccdb592af2ca0e79d7d
Ed Brooksbank <ejb66@le.ac.uk>
2019-09-20 10:54:28 +0100

    small change to openGL added new preferences for proxy settings

--- __main__.py
+++ __main__.py
@@ -22,16 +22,13 @@ __date__ = "$Date: 2017-04-07 10:28:45 +0000 (Fri, April 07, 2017) $"
 # Start of code
 #=========================================================================================
 
-
 from ccpn.framework import Framework
 from ccpn.AnalysisMetabolomics.AnalysisMetabolomics import Metabolomics as Application
 from ccpn.framework.Version import applicationVersion
 
 
 if __name__ == '__main__':
-    from ccpn.util.GitTools import getAllRepositoriesGitCommit
-
-
+    # from ccpn.util.GitTools import getAllRepositoriesGitCommit
     # applicationVersion = 'development: {AnalysisMetabolomics:.8s}'.format(**getAllRepositoriesGitCommit())
 
     # argument parser
@@ -42,4 +39,5 @@ if __name__ == '__main__':
 
     application = Application(Framework.AnalysisMetabolomics, applicationVersion, commandLineArguments)
     Framework._getApplication = lambda: application
+
     application.start()
